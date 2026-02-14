3ae2c9a68442e90a126e9faf4c7cc3ca0a451190
robpatterson13 <robertpatterson1313@gmail.com>
2026-02-13 20:34:36 -0500

    Initial Commit

---
+++ HackBeanpot2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackBeanpot2026
+//
+//  Created by Rob Patterson on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
